3c3a5b1a84ad05519f6c565d628bae247413ce70
OrganTrouble <jessedavda@gmail.com>
2018-03-31 15:05:08 +0100

    sorting out main project showcase

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
     $(document).scroll(() => {
-        $("nav").css('background-color', 'rgba(0, 0, 0, 0.2)'); 
+        $("nav").css('background-color', 'rgba(0, 0, 0, 0.5)'); 
     });
 });
