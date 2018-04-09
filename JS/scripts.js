05186a989cfbc751a4b6bdb9f3de7483d3138814
OrganTrouble <jessedavda@gmail.com>
2018-04-09 18:50:25 +0100

    added the navbar opacity and changed a few things

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
-    $(document).scroll(() => {
-        $("nav").css('background-color', 'rgba(0, 0, 0, 0.5)'); 
-    });
+
+    var navScroll = new Waypoint({
+        element: document.getElementById('scroll_waypoint'),
+        handler: () => {
+            $('nav').css("backgroundColor", "rgba(0, 0, 0, 0.3)");
+        }
+    })
+
 });
