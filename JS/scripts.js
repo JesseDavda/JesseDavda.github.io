b9a5cedc79222386f5e5ca92aef8090344f19056
OrganTrouble <jessedavda@gmail.com>
2018-04-09 19:07:40 +0100

    fixed the scrolling

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,8 +2,12 @@ $(document).ready(() => {
 
     var navScroll = new Waypoint({
         element: document.getElementById('scroll_waypoint'),
-        handler: () => {
-            $('nav').css("backgroundColor", "rgba(0, 0, 0, 0.3)");
+        handler: (direction) => {
+            if(direction == "down") {
+                $('nav').css("backgroundColor", "rgba(0, 0, 0, 0.3)");
+            } else if(direction == "up") {
+                $('nav').css("backgroundColor", "rgba(0, 0, 0, 0)");
+            }
         }
     })
 
